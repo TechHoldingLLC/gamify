76cbc2a0a16ee17c4f7d307af59befb3133a7402
Devang Naghera <devang.naghera@techholding.co>
2019-11-27 11:31:08 +0530

    Update hooks tests

--- src/hooks/useGameState.test.jsx
+++ src/hooks/useGameState.test.jsx
@@ -1,13 +1,15 @@
 /* eslint-disable react/jsx-handler-names */
 import React from 'react';
 import { mount } from 'enzyme';
-import { MemoryRouter } from 'react-router';
 
-// import { act } from 'react-dom/test-utils';
-// import wait from 'waait';
+import { act } from 'react-dom/test-utils';
+
 import Game from '../pages/Game';
+import Cards from '../pages/Game/Cards';
 
 jest.useFakeTimers();
+
+jest.mock('../components/Anchor', () => ({ children }) => <>{children}</>);
 
 describe('useGameState hook', () => {
   beforeEach(() => {
@@ -19,11 +21,43 @@ describe('useGameState hook', () => {
     },
   };
   it('Should render without crashing', async () => {
-    mount(
-      <MemoryRouter>
-        {' '}
-        <Game match={match} />
-      </MemoryRouter>,
-    );
+    mount(<Game match={match} />);
+  });
+
+  it('should start playing game on play button click', () => {
+    const wrapper = mount(<Game match={match} />);
+    const playButton = wrapper.find('.playButton');
+    playButton.first().simulate('click');
+    wrapper.update();
+    expect(wrapper.find('.card').exists()).toBe(true);
+  });
+
+  it('should call stop function on timeout', async () => {
+    const wrapper = mount(<Game match={match} />);
+    const playButton = wrapper.find('.playButton');
+    playButton.first().simulate('click');
+    await act(() => {
+      wrapper.update();
+      jest.runAllTimers();
+      wrapper.update();
+    });
+    expect(wrapper.find('.card').exists()).toBe(false);
+  });
+
+  it('should flip card on click', async () => {
+    const wrapper = mount(<Game match={match} />);
+    const playButton = wrapper.find('.playButton');
+    playButton.first().simulate('click');
+    jest.advanceTimersByTime(10000);
+    await act(() => wrapper.update());
+    await act(() => {
+      wrapper
+        .find('.card')
+        .first()
+        .simulate('click');
+
+      wrapper.update();
+    });
+    console.log(wrapper.find(Cards).props());
   });
 });

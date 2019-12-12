78550e4c7eba3a50025185c945fa09d0b95f1ddf
Devang Naghera <devang.naghera@techholding.co>
2019-12-12 14:25:09 +0530

    Fixed coverage

--- src/pages/Game/Game.test.jsx
+++ src/pages/Game/Game.test.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
+import { act } from 'react-dom/test-utils';
 import Game from './index';
+
+jest.useFakeTimers();
+
+jest.mock('../../components/Anchor', () => ({ children }) => <>{children}</>);
 
 describe('Game component', () => {
   beforeEach(() => {
@@ -14,5 +19,24 @@ describe('Game component', () => {
 
   it('renders without crashing', () => {
     shallow(<Game match={match} />);
+  });
+
+  it('should start playing game on play button click', () => {
+    const wrapper = mount(<Game match={match} />);
+    const playButton = wrapper.find('.playButton');
+    playButton.first().simulate('click');
+    wrapper.update();
+    expect(wrapper.find('.card').exists()).toBe(true);
+  });
+  it('should call stop game on timeout', async () => {
+    const wrapper = mount(<Game match={match} />);
+    const playButton = wrapper.find('.playButton');
+    playButton.first().simulate('click');
+    act(() => {
+      wrapper.update();
+      jest.runAllTimers();
+      wrapper.update();
+    });
+    expect(wrapper.find('.card').exists()).toBe(false);
   });
 });
